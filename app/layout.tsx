b026436272c37b8f272d871674cd19990dbc0776
Sumit Singh <sumitsingh@Sumits-MacBook-Air.local>
2025-12-30 16:00:44 +0530

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,19 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Gerox",
+  description: "Gerox",
+  icons: {
+    icon: [
+      { url: "/favicon.png", type: "image/png" }, // or "/favicon.svg"
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", type: "image/png" }, // optional
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
